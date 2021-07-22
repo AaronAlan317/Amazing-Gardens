2dd333611f9325a97284f4b69354e5799f13ae32
Aaron <AaronAlanHernandez@gmail.com>
2021-07-21 22:26:35 -0700

    removed proxy

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { reducers } from './reducers';
 import App from './App';
 import './index.css';
+import axios from "axios";
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
