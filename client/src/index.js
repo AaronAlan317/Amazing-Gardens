071ee03e448b26e7baf113f56037feeabe3a58b1
Aaron <AaronAlanHernandez@gmail.com>
2021-07-21 22:22:06 -0700

    added .env

--- client/src/index.js
+++ client/src/index.js
@@ -10,6 +10,14 @@ import './index.css';
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
+if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+  // development URL
+  axios.defaults.baseURL = `${process.env.REACT_APP_API_SERVER_DEVELOPMENT}/api/v1`;
+} else {
+  // production URL
+  axios.defaults.baseURL = `${process.env.REACT_APP_API_SERVER_PRODUCTION}/api/v1`;
+}
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
